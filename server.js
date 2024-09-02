4126d815a46440aea91001ead42ad7e573319a9b
Willemien van Tonder <willemienvz@gmail.com>
2024-09-02 22:37:20 +0200

    Changes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
